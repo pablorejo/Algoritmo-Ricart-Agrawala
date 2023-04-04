8d63a1d121169128ea2a6849f98ecff87bf0fb78
pablorejo <42997285+pablorejo@users.noreply.github.com>
2023-04-04 11:13:28 +0200

    Hay que probarlo

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 #include <sys/msg.h> // Para usar la funcion
 
 #define N 3 
-#define MAX_MSG_SIZE 1000
 
 int mi_ticket = 0, mi_id, id_nodos_pend[N-1] = {0}, num_pend = 0, quiero = 0, max_ticket = 0;
 
@@ -36,17 +35,41 @@ int main(int argc, char const *argv[])
         exit(-1);
     }
     int msgid = msgget(1,0664 | IPC_CREAT); // Creamos el buzón
-    // pthread_create(, NULL, recivir, (void *) );
-    int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
 
+
+    msg.id_nodo_origen = mi_id;
+    msg.ticket_origen = 0;
+
+
+    int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
     while (1)
     {
-        for (int i = 0; i < N-1; i++) send(REQUEST, id_nodos[i], {mi_id, mi_ticket});
-        for (int i = 0; i < N-1; i++) receive(REPLY, &id_aux);
-        SECCIÓN CRÍTICA;
+        printf("Pulsa enter para entrar en la sección crítica\n");
+        while (getchar()!='\n'){}
+        
+        // Semaforo de exclusión mutua aquí
+        msg.ticket_origen = msg.ticket_origen + 1;
+        // Termina el semaforo
+
+        for (int i = 0; i < N-1; i++) {
+            //Enviamos un mensaje a todos los nodos diciendo que queremos entrar en la sección crítica
+            msgsnd(msgid, &msg, 2*sizeof(int), i); //Enviamos el mensaje al nodo origen
+        }
+        for (int i = 0; i < N-1; i++){
+            // Intentaresmo recivir respuesta de todos los nodos y cuando esto pase iremos a nuestra sección crítica
+           
+            msgrcv(msgid, &msg, 2*sizeof(int), mi_id, 0); //Recivimos el mensaje
+        } 
+        ///SECCIÓN CRÍTICA;
+        printf("Haciendo la sección crítica\n");
+
+
+
         quiero = 0;
-        for (i = 0; i < num_pend; i++)
-        // send(REPLY, id_nodos_pend[i], mi_id);
+        for (int i = 0; i < num_pend; i++){
+            // Enviamos los mensajes que nos quedasen pendientes de enviar
+            msgsnd(msgid, &msg, 2*sizeof(int), i); //Enviamos el mensaje al nodo origen
+        }
         num_pend = 0;
     }
 
@@ -57,18 +80,28 @@ int main(int argc, char const *argv[])
 void* recivir(void *args) {
 
     while (1) {
-        
-        msgrcv(msgid, &msg, MAX_MSG_SIZE, mi_id, 0);
-        if (msg.ticket_origen < mi_ticket){
-            msgsnd(msgid, &msg, 2*sizeof(int), msg.id_nodo_origen); //Enviamos el mensaje al nodo origen
-        } 
-        else{
-            id_nodos_pend[num_pend++] = msg.id_nodo_origen ;
-        }
+        // recivimos una petición
+        msgrcv(msgid, &msg, 2*sizeof(int), mi_id, 0);
 
-        max_ticket = MAX(max_ticket, ticket_origen);
-        if (NOT quiero OR ticket_origen < mi_ticket || (ticket_origen == mi_ticket && (id_nodo_origen < mi_id))
-        send(REPLY, id_nodo_origen, mi_id);
-        else id_nodos_pend[num_pend++] = id_nodo_origen;
+        // if (msg.ticket_origen < mi_ticket){
+        //     msgsnd(msgid, &msg, 2*sizeof(int), msg.id_nodo_origen); //Enviamos el mensaje al nodo origen
+        // } 
+        // else{
+        //     id_nodos_pend[num_pend++] = msg.id_nodo_origen ;
+        // }
+
+        // Semaforo de exclusión mutua aquí
+        max_ticket = MAX(max_ticket, msg.ticket_origen);
+
+        if (quiero = 0 || msg.ticket_origen < mi_ticket || (msg.ticket_origen == mi_ticket && (msg.id_nodo_origen < mi_id))){
+            // En caso de que no queramos enviar un ticket quiero = 0
+            // En caso de que el ticket recivido sea menor que nuestro ticket
+            // Si nuestro ticket es igual al recivido pero nuestro id es mayor que el del origen
+            msgsnd(msgid, &msg, 2*sizeof(int), msg.id_nodo_origen); //Enviamos el mensaje al nodo origen
+        }
+        else {
+            id_nodos_pend[num_pend++] = msg.id_nodo_origen;
+        }
+        // Termina el semaforo de exclusion mutua
     }
 }
