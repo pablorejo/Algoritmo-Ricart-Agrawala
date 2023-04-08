c678162018340eeadd5b4a95c6058e9380108c2e
pablorejo <pablopiorejoiglesias@gmail.com>
2023-04-08 12:34:11 +0200

    Mejorando cosas

--- ejecutar.sh
+++ ejecutar.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 gcc main.c -o main -Wall
-for ((i=1;i<=$1;i++))
-do
-    ./main $1
-done
+./main $1 $2
+
+
+# for ((i=1;i<=$1;i++))
+# do
+#     ./main $i $1
+# done
--- main.c
+++ main.c
@@ -8,34 +8,40 @@
 #include <sys/msg.h> // Para usar la funcion
 
 #include <semaphore.h>
-#define N 3 
+#define N 1000
 
-int mi_ticket = 0, mi_id, id_nodos_pend[N-1] = {0}, num_pend = 0, quiero = 0, max_ticket = 0;
-
-pthread_t mi_id_thread;
-
+int mi_ticket = 0, mi_id, id_nodos_pend[N-1] = {0}, num_pend = 0, quiero = 0, max_ticket = 0, n_procesos = N-1;
 int msgid; //id del buzón
 
-void* recivir(void *);
+int id_nodos[N-1] = {0};//Id de todos los nodos
+
 
 typedef struct 
 {
     int id_nodo_origen;
     int ticket_origen;
 }mensaje;
-
 mensaje msg;
 
 sem_t mutex;
+pthread_t mi_id_thread;
+
+
+void* recivir(void *);
 
 int main(int argc, char const *argv[])
 {
-    if (argc < 2){
-        mi_id = argv[1]; // Guardamos el id de nuestro que nos otorgara el usuario
-    }else
-    {
-        printf("Introduce el id\n");
+    if (argc < 3){
+        printf("Introduce el id y cuantos procesos hay\n");
         exit(-1);
+    }else{
+        mi_id = atoi(argv[1]); // Guardamos el id que nos otorgara el usuario    
+        n_procesos = atoi(argv[2]); // Numero de procesos totales
+
+        // Guardando ids de los procesos
+        for (int i = 0; i < n_procesos; i++){
+            id_nodos[i] = i;
+        }
     }
     int msgid = msgget(1,0664 | IPC_CREAT); // Creamos el buzón
 
@@ -48,24 +54,29 @@ int main(int argc, char const *argv[])
 
 
     int pthread_create(pthread_t *thread, const pthread_attr_t *attr, void *(*start_routine) (void *), void *arg);
-    while (1)
-    {
+    while (1){
         printf("Pulsa enter para entrar en la sección crítica\n");
         while (getchar()!='\n'){}
-        
+        printf("Intentando entrar a la sección crítica\n");
         // Semaforo de exclusión mutua aquí
         sem_wait(&mutex);
-        msg.ticket_origen = msg.ticket_origen + 1;
+        quiero = 1;
+        msg.ticket_origen = max_ticket + 1;
         sem_post(&mutex);
         // Termina el semaforo
 
-        for (int i = 0; i < N-1; i++) {
+        printf("Enviando mensajes para a todos los nodos\n");
+        for (int i = 0; i < n_procesos; i++) {
             //Enviamos un mensaje a todos los nodos diciendo que queremos entrar en la sección crítica
-            msgsnd(msgid, &msg, 2*sizeof(int), i); //Enviamos el mensaje al nodo origen
+            if (id_nodos[i] != mi_id){
+                msgsnd(msgid, &msg, 2*sizeof(int), id_nodos[i]); //Enviamos el mensaje al nodo origen
+            }
         }
-        for (int i = 0; i < N-1; i++){
+        printf("Reciviendo mensajes para a todos los nodos\n");
+
+        // Tenemos que recivir mensajes de todos los nodos para poder ejecutar la sección crítica
+        for (int i = 0; i < n_procesos; i++){
             // Intentaresmo recivir respuesta de todos los nodos y cuando esto pase iremos a nuestra sección crítica
-           
             msgrcv(msgid, &msg, 2*sizeof(int), mi_id, 0); //Recivimos el mensaje
         } 
         ///SECCIÓN CRÍTICA;
@@ -80,29 +91,22 @@ int main(int argc, char const *argv[])
         }
         num_pend = 0;
     }
-
 }
 
 
 // Creamos la funcion que se encargará de recivir los mensajes
 void* recivir(void *args) {
-
     while (1) {
         // recivimos una petición
         msgrcv(msgid, &msg, 2*sizeof(int), mi_id, 0);
 
-        // if (msg.ticket_origen < mi_ticket){
-        //     msgsnd(msgid, &msg, 2*sizeof(int), msg.id_nodo_origen); //Enviamos el mensaje al nodo origen
-        // } 
-        // else{
-        //     id_nodos_pend[num_pend++] = msg.id_nodo_origen ;
-        // }
 
         // Semaforo de exclusión mutua aquí
         sem_wait(&mutex);
-        max_ticket = MAX(max_ticket, msg.ticket_origen);
+        // asignamos el valor maximo a ticket maximo
+        if (msg.ticket_origen > max_ticket){ max_ticket = msg.ticket_origen; }
 
-        if (quiero = 0 || msg.ticket_origen < mi_ticket || (msg.ticket_origen == mi_ticket && (msg.id_nodo_origen < mi_id))){
+        if (quiero == 0 || msg.ticket_origen < mi_ticket || (msg.ticket_origen == mi_ticket && (msg.id_nodo_origen < mi_id))){
             // En caso de que no queramos enviar un ticket quiero = 0
             // En caso de que el ticket recivido sea menor que nuestro ticket
             // Si nuestro ticket es igual al recivido pero nuestro id es mayor que el del origen
